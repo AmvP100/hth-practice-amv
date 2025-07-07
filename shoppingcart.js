5c2f235a0f10d2416c844675fd4d13901b990702
Amvina Pham <215578691+AmvP100@users.noreply.github.com>
2025-07-07 14:41:28 -0700

    update js code

---
+++ shoppingcart.js
@@ -0,0 +1,10 @@
+const storeProducts = {
+  apples: 1.5,
+  bread: 2.0,
+  milk: 2.5,
+  eggs: 3.0,
+  cheese: 4.0
+};
+
+// 2. Shopping cart array (items by name)
+const shoppingCart = ['apples', 'milk', 'cheese', 'eggs'];
